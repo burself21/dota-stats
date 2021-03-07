d6b67dcaaba416bcd1b3cb2b6fb1cd8c211bc8f6
Nathan Johns <nathanjohns@berkeley.edu>
2021-03-06 20:45:27 -0700

    Updates

---
+++ precache-manifest.6f4e5d64c4cee4e22611c7f8024b3d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "911027dd956acce3bab6",
+    "url": "/dota-stats/static/css/main.9b3b3c10.chunk.css"
+  },
+  {
+    "revision": "911027dd956acce3bab6",
+    "url": "/dota-stats/static/js/main.f19fc676.chunk.js"
+  },
+  {
+    "revision": "24d3da52c66c29e760b8",
+    "url": "/dota-stats/static/js/runtime~main.5248668e.js"
+  },
+  {
+    "revision": "cc1c9f36e3065b9ae3f7",
+    "url": "/dota-stats/static/js/2.5277dd08.chunk.js"
+  },
+  {
+    "revision": "8a27eee94acf3c9ae8bdfe7419780825",
+    "url": "/dota-stats/static/media/clock1.8a27eee9.png"
+  },
+  {
+    "revision": "1dfd89e74350d6211866f641c7378921",
+    "url": "/dota-stats/index.html"
+  }
+];
